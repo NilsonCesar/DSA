bfe4c67bc7a46e11de3ce2844e24d7a033c3b9f4
nilsoncesar <admncoaf@gmail.com>
2026-01-07 12:03:01 -0300

    [Heap] create heapify_down

--- DataStructures/Miscellaneous/Heap.cpp
+++ DataStructures/Miscellaneous/Heap.cpp
@@ -7,14 +7,25 @@ void heapify_up(vector<int>& heap, int i) {
     if (i > 1) {
         int j = i / 2;
 
-        // cout << i << " " << j << '\n';
-        // cout << heap[i] << " " << heap[j] << '\n';
         if (heap[j] > heap[i]) {
-            int c = heap[i];
-            heap[i] = heap[j];
-            heap[j] = c;
+            swap(heap[i], heap[j]);
             heapify_up(heap, j);
         }
+    }
+}
+
+void heapify_down(vector<int>& heap, int i, int n) {
+    if (2 * i > n) return;
+    int j = 2 * i;
+    if (2 * i < n) {
+        if (heap[2 * i] > heap[2 * i + 1]) {
+            j = 2 * i + 1;
+        } 
+    }
+
+    if (heap[j] < heap[i]) {
+        swap(heap[i], heap[j]);
+        heapify_down(heap, j, n);
     }
 }
 
@@ -27,9 +38,25 @@ int main(void) {
         heap[n] = num;
         heapify_up(heap, n); n++;
     }
+    n--;
 
-    for (int i = 1; i <= 10; i++) {
-        cout << heap[i] << '\n';
+    cout << "heap:" << '\n';
+    for (int i = 1; i <= n; i++) {
+        cout << heap[i] << ' ';
     }
+    cout << '\n';
+
+    vector<int> sorted_a(10);
+    for (int i = 0; i < 10; i++) {
+        sorted_a[i] = heap[1];
+        swap(heap[1], heap[n]);
+        heapify_down(heap, 1, --n);
+    }
+
+    cout << "Array sorted by heapsort:" << '\n';
+    for (int i = 0; i < 10; i++) {
+        cout << sorted_a[i] << ' ';
+    }
+    cout << '\n';
 
 }
